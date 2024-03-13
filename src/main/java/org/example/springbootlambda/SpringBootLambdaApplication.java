66edd20b7cd9cb13e526c122db54722052c5a182
mynspluto <mynspluto@MacBook-Pro-2.local>
2024-03-13 18:16:15 +0900

    first commit

---
+++ src/main/java/org/example/springbootlambda/SpringBootLambdaApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class SpringBootLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLambdaApplication.class, args);
+	}
+
+}
